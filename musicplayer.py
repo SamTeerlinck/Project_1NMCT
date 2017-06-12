ebfc5ed0dfd63acbb4a3f71bb9bfad348eaa7cc0
SamTeerlinck <sammyteer@gmail.com>
2017-06-12 19:20:39 +0200

    Create musicplayer.py

--- musicplayer.py
+++ musicplayer.py
@@ -1,6 +1,10 @@
+#Music player test
+
 import pygame
+
 pygame.mixer.init()
 pygame.mixer.music.load("myFile.wav")
 pygame.mixer.music.play()
+
 while pygame.mixer.music.get_busy() == True:
     continue
